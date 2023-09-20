90b5b08aa9ac00e6745df970c11238aae09632e1
lukas <lukas.weber.4.12@gmail.com>
2023-09-21 00:32:50 +0200

    add single stack item types

--- ender_core/src/inventory/item.rs
+++ ender_core/src/inventory/item.rs
@@ -6,14 +6,72 @@ pub enum ItemStack {
     Full(FullItemStack),
 }
 
-pub enum SingleItemStack {}
+pub enum SingleItemStack {
+    MineCart,
+    Boat,
+    WaterBucket,
+    LavaBucket,
+    EnchantedBook {
+        enchantment: Enchantment
+    },
+    SignedBook {
+        name: String
+    },
+    Tool {
+        tool_type: ToolType,
+        material: ToolMaterial,
+    },
+    Armor {
+        armor_type: ArmorType,
+        material: ArmorMaterial,
+    },
+}
+
+pub enum ToolType {
+    Pickaxe,
+    Axe,
+    Sword,
+    Shovel,
+    Hoe,
+}
+
+pub enum ToolMaterial {
+    Wood,
+    Stone,
+    Iron,
+    Gold,
+    Diamond,
+    Netherite,
+}
+
+pub enum ArmorType {
+    Hat,
+    ChestPlate,
+    Pants,
+    Boots,
+}
+
+pub enum ArmorMaterial {
+    Leather,
+    Chain,
+    Iron,
+    Gold,
+    Diamond,
+    Netherite,
+}
+
+pub enum Enchantment {}
 
 pub struct QuarterItemStack {
     item_type: QuarterItemStackType,
     amount: Bounded1To16,
 }
 
-pub enum QuarterItemStackType {}
+pub enum QuarterItemStackType {
+    Egg,
+    EnderPerl,
+    EmptyBucket,
+}
 
 pub struct FullItemStack {
     item_type: FullItemStackType,
